df1155327d63fc096e2f97de82b5cd70021a5144
Shirong Foo <dee.sr.foo@gmail.com>
2017-05-30 19:34:13 +0800

    more debuggisg

--- routes/main.js
+++ routes/main.js
@@ -33,27 +33,27 @@ Product.ping({
 )
 
 // function to paginate mongoose query
-// function paginate (req, res, next) {
-//   var perPage = 9
-//   var page = req.params.page
-//
-//   Product
-//   .find()
-//   .skip(perPage * page)
-//   .limit(perPage)
-//   .populate('category')
-//   .exec(function (err, products) {
-//     if (err) return next(err)
-//     Product.count().exec(function (err, count) {
-//       if (err) return next(err)
-//       res.render('main/product-main', {
-//         products: products,
-//         pages: count / perPage,
-//         error: req.flash('errors')
-//       })
-//     })
-//   })
-// }
+function paginate (req, res, next) {
+  var perPage = 9
+  var page = req.params.page
+
+  Product
+  .find()
+  .skip(perPage * page)
+  .limit(perPage)
+  .populate('category')
+  .exec(function (err, products) {
+    if (err) return next(err)
+    Product.count().exec(function (err, count) {
+      if (err) return next(err)
+      res.render('main/product-main', {
+        products: products,
+        pages: count / perPage,
+        error: req.flash('errors')
+      })
+    })
+  })
+}
 
 // createMapping creates map between mongoLab and elasticSearch replica set
 // Product.createMapping(function (err, mapping) {
@@ -71,16 +71,16 @@ Product.ping({
 // get data from server: homepage
 router.get('/', function (req, res, next) {
   if (req.user) {
-    // paginate(req, res, next)
-    res.send('testing')
+    paginate(req, res, next)
+    // res.send('testing')
   } else {
     res.render('main/home', {error: req.flash('errors') })
   }
 })
 
-// router.get('/page/:page', function (req, res, next) {
-//   paginate(req, res, next)
-// })
+router.get('/page/:page', function (req, res, next) {
+  paginate(req, res, next)
+})
 
 // post data to server: search queries
 router.post('/search', function (req, res, next) {
